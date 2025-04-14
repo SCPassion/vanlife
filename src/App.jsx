a54a9ec4f0b1f04a45d0f0ebd6149161f80d6028
SCPassion <141584825+SCPassion@users.noreply.github.com>
2025-04-14 21:19:04 +0200

    Implement the basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Routes, Route, NavLink } from "react-router"
+import Layout from "./components/Layout"
+
 function App() {
   return (
     <>
-      <h1>hi</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route index element={<h1>Home</h1>} />
+            <Route path="about" element={<h1>About</h1>} />
+            <Route path="vans" element={<h1>Vans</h1>} />
+            <Route path="vans/:id" element={<h1>Van Details</h1>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
