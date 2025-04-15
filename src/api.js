d3c8a55da91f18758e4f4c71e832b8ae7ef4482c
SCPassion <141584825+SCPassion@users.noreply.github.com>
2025-04-15 21:42:14 +0200

    Fetch and display van data in Vans component; add Firebase API integration

---
+++ src/api.js
@@ -0,0 +1,34 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+import { getAnalytics } from "firebase/analytics"
+import { collection, getDocs, getFirestore } from "firebase/firestore"
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA3RVX4z9gMSBH0_I4-jmNIKh47oihaQ9M",
+  authDomain: "vanlife-5c90a.firebaseapp.com",
+  projectId: "vanlife-5c90a",
+  storageBucket: "vanlife-5c90a.firebasestorage.app",
+  messagingSenderId: "10779121089",
+  appId: "1:10779121089:web:6de72c8993ee71ef0d4b40",
+  measurementId: "G-24ZXDJCC85",
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
+const db = getFirestore(app)
+
+export async function getAllVans() {
+  const vansCol = collection(db, "vans")
+  const snapshot = await getDocs(vansCol)
+  const vans = snapshot.docs.map((doc) => ({
+    ...doc.data(),
+    id: doc.id,
+  }))
+  return vans
+}
--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,9 +1,28 @@
 import { NavLink, useLocation } from "react-router"
 import { useState, useEffect } from "react"
+import { getAllVans } from "../api"
 
 import clsx from "clsx"
+//  {
+// hostId: "123",
+// price: 60,
+// imageUrl: "https://assets.scrimba.com/advanced-react/react-router/modest-explorer.png",
+// description: "The Modest Explorer is a van designed to get you out of the house and into nature. This beauty is equipped with solar panels, a composting toilet, a water tank and kitchenette. The idea is that you can pack up your home and escape for a weekend or even longer!",
+// type: "simple",
+// name: "Modest Explorer", id: "1" }
+
 export default function Vans() {
   const [vans, setVans] = useState([])
+
+  useEffect(() => {
+    async function fetchVans() {
+      const vansData = await getAllVans()
+      console.log(vansData)
+      setVans(vansData)
+    }
+
+    fetchVans()
+  }, [])
 
   const location = useLocation()
   const searchParams = new URLSearchParams(location.search)
@@ -13,6 +32,34 @@ export default function Vans() {
     "rounded-sm bg-[#FFEAD0] px-7 py-1.5 font-medium text-[#4D4D4D]"
 
   const selectClassName = clsx(normalClassName, "bg-black text-white")
+
+  const typeBackgroundColor = {
+    simple: "bg-[#E17654]",
+    luxury: "bg-[#115E59]",
+    rugged: "bg-[#161616]",
+  }
+
+  const vanElements = vans.map((van) => (
+    <div key={van.id}>
+      <img
+        src={van.imageUrl}
+        alt={van.name}
+        className="mb-2.5 size-57 rounded-md"
+      />
+      <div className="flex justify-between">
+        <p>{van.name}</p>
+        <p>
+          ${van.price} <br />
+          /day
+        </p>
+      </div>
+      <div
+        className={`${typeBackgroundColor[van.type]} -mt-5 flex h-9 w-21 items-center justify-center rounded-md text-base font-semibold text-[#FFEAD0]`}
+      >
+        {van.type}
+      </div>
+    </div>
+  ))
 
   return (
     <section className="px-7 py-14">
@@ -40,6 +87,7 @@ export default function Vans() {
           Clear filters
         </NavLink>
       </nav>
+      <div className="flex flex-wrap justify-center gap-8.5">{vanElements}</div>
     </section>
   )
 }
