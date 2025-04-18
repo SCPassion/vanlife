98b52294312f2736bcdee484d6832104285c026f
SCPassion <141584825+SCPassion@users.noreply.github.com>
2025-04-18 22:52:19 +0200

    Enhance navigation: add useNavigate for redirecting to home after successful sign-in

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,13 @@
 import Button from "../components/Button"
 import { signUp, signIn } from "../api"
 import { useState } from "react"
+import { replace, useNavigate } from "react-router"
 
 export default function Login() {
   const [isSignUp, setIsSignUp] = useState(false)
   const [error, setError] = useState(null)
+  const navigate = useNavigate()
+
   function toggleSignUp() {
     setIsSignUp((prev) => !prev)
   }
@@ -22,6 +25,7 @@ export default function Login() {
         const user = await signIn({ email, password })
         console.log("user signed in: ", user)
         setError(null)
+        navigate("/", { replace: true })
       }
     } catch (error) {
       console.error("Error:", error)
