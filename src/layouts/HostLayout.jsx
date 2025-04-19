7a547b245cfaa87c25ee84f96143345b248e131e
SCPassion <141584825+SCPassion@users.noreply.github.com>
2025-04-19 22:24:30 +0200

    Refactor HostLayout and Dashboard components: adjust layout styles and implement Dashboard content

--- src/layouts/HostLayout.jsx
+++ src/layouts/HostLayout.jsx
@@ -3,8 +3,8 @@ import { NavLink, Outlet } from "react-router"
 export default function HostLayout() {
   const activeClass = "underline font-bold text-[#161616]"
   return (
-    <div className="px-6.5">
-      <nav className="mb-11.25 flex gap-7.25 text-lg font-medium text-[#4D4D4D]">
+    <div>
+      <nav className="mb-11.25 flex gap-7.25 px-6.5 text-lg font-medium text-[#4D4D4D]">
         <NavLink
           to="."
           relative="path"
--- src/pages/host/Dashboard.jsx
+++ src/pages/host/Dashboard.jsx
@@ -1,3 +1,29 @@
+import { FaStar } from "react-icons/fa"
+
 export default function () {
-  return <h1>Dashboard here</h1>
+  return (
+    <section className="text-[#4D4D4D]">
+      <div className="flex flex-col gap-8 bg-[#FFEAD0] px-6.5 py-9.25">
+        <h1 className="text-4xl font-bold text-[#161616]">Welcome!</h1>
+        <div className="flex justify-between text-base font-medium">
+          <p>
+            Income last <span className="font-bold underline">30 days</span>
+          </p>
+          <p>Details</p>
+        </div>
+        <p className="text-5xl font-extrabold text-[#161616]">$2,260</p>
+      </div>
+
+      <div className="flex items-center justify-between bg-[#FFDDB2] px-6.5 py-11.25">
+        <div className="flex items-center text-2xl">
+          <p className="mr-3 font-bold text-[#161616]">Review score</p>
+          <FaStar className="mr-2 fill-[#FF8C38]" />
+          <p>
+            <span className="font-bold text-[#161616]">5.0</span>/5
+          </p>
+        </div>
+        <p className="text-base text-[#161616]">Details</p>
+      </div>
+    </section>
+  )
 }
