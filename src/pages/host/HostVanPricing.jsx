ee4760d146478c5ad1c246c18905bc4b9ad52c2e
SCPassion <141584825+SCPassion@users.noreply.github.com>
2025-04-20 16:13:15 +0200

    Implement pricing display for HostVanPricing component using Outlet context

--- src/pages/host/HostVanPricing.jsx
+++ src/pages/host/HostVanPricing.jsx
@@ -1,3 +1,16 @@
+import { useOutletContext } from "react-router"
+
 export default function HostVanPricing() {
-  return <h1>HostVanPricing</h1>
+  const { van } = useOutletContext()
+
+  return (
+    van && (
+      <p className="text-base font-medium text-[#4D4D4D]">
+        <span className="text-2xl text-[#161616]">
+          ${Number(van.price).toFixed(2)}
+        </span>
+        /day
+      </p>
+    )
+  )
 }
