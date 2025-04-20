d0fdf6d0aa26e049507a0f7e67364f246f4c0cd7
SCPassion <141584825+SCPassion@users.noreply.github.com>
2025-04-20 20:14:52 +0200

    Refactor Income component to enhance layout and display transaction details

--- src/pages/host/Income.jsx
+++ src/pages/host/Income.jsx
@@ -1,3 +1,18 @@
 export default function Income() {
-  return <h1>Income here.</h1>
+  return (
+    <section className="px-6.5 text-[#161616]">
+      <h1 className="mb-11 text-4xl font-bold">Income</h1>
+      <p className="mb-7.5 font-medium text-[#4D4D4D]">
+        Last <span className="font-semibold underline">30 days</span>
+      </p>
+      <p className="mb-14.25 text-5xl font-extrabold">$2,260</p>
+
+      <div className="flex">
+        <h3>Your transactions (3)</h3>
+        <p>
+          Last <span>30 days</span>
+        </p>
+      </div>
+    </section>
+  )
 }
