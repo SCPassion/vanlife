89cddc96dcec80bfb7e707bc970f6789ef9bf074
SCPassion <141584825+SCPassion@users.noreply.github.com>
2025-04-21 17:13:54 +0200

    Remove console log from Transaction component

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,5 +1,4 @@
 export default function Transaction({ revenue, date }) {
-  console.log(revenue)
   return (
     <div className="flex items-center justify-between rounded-md bg-white px-7.25 py-8.75 last:mb-15.5">
       <p className="text-4xl font-semibold text-[#161616]">${revenue}</p>
