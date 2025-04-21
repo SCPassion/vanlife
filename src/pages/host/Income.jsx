f4ee30a30a93b4ad7894084f8cf35ebc6227bf66
SCPassion <141584825+SCPassion@users.noreply.github.com>
2025-04-21 17:13:36 +0200

    Refactor Income component layout and integrate Transaction component for better transaction display

--- src/pages/host/Income.jsx
+++ src/pages/host/Income.jsx
@@ -1,6 +1,6 @@
 import chartData from "../../chartData"
 import Chart from "../../components/Chart"
-
+import Transaction from "../../components/Transaction"
 export default function Income() {
   return (
     <section className="px-6.5 text-[#161616]">
@@ -12,11 +12,18 @@ export default function Income() {
 
       <Chart chartData={chartData} variable="income" />
 
-      <div className="flex">
-        <h3>Your transactions (3)</h3>
-        <p>
-          Last <span>30 days</span>
-        </p>
+      <div className="mt-16 flex flex-col gap-7.75">
+        <div className="flex items-center justify-between">
+          <h3 className="text-2xl font-bold text-[#161616]">
+            Your transactions (3)
+          </h3>
+          <p className="text-base font-medium text-[#4D4D4D]">
+            Last <span className="text-[#161616] underline">30 days</span>
+          </p>
+        </div>
+        <Transaction revenue={720} date="1/12/22" />
+        <Transaction revenue={560} date="10/11/22" />
+        <Transaction revenue={980} date="23/11/22" />
       </div>
     </section>
   )
